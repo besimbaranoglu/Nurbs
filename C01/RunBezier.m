2156a29876226ae495665b439473d51edc11fbaf
Besim Baranoglu <besimbaranoglu@gmail.com>
2020-06-07 08:54:39 +0300

    up to rational bezier

--- C01/RunBezier.m
+++ C01/RunBezier.m
@@ -1,13 +1,15 @@
-%This is a main routine to run Horner's Algorithm
+%This is a main routine to run Bezier curve
 clear;
 
 %determine the order of the function
-n = input('Enter the order of the polynomial: ');
+n = input('Enter the order of the Bezier curve: ');
+n = n +1;
+% u = input('Enter the value of u: ');
 
 %we should add n points (vectors) to the system
-A = zeros(n+1,3);
+A = zeros(n,3);
 
-for i=1:n+1
+for i=1:n
     fprintf('Enter x-coordinate of point %d : ',i); A(i,1) = input('');
     fprintf('Enter y-coordinate of point %d : ',i); A(i,2) = input('');
     fprintf('Enter z-coordinate of point %d : ',i); A(i,3) = input('');
@@ -15,9 +17,10 @@ end
 PX = zeros(100,1);
 PY = zeros(100,1);
 PZ = zeros(100,1);
-for i=1:100
-    u0 = i/100;
-    [C] = Horner1(A, n, u0);
+% [B] = AllBernstein(n,u);
+for i=1:101
+    u0 = (i-1)/100;
+    [C] = PointOnBezierCurve(A,n,u0);
     PX(i) = C(1);
     PY(i) = C(2);
     PZ(i) = C(3);
